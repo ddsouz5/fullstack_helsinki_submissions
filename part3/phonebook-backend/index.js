c4f28be5732a029b35de305adbdc2f1962ea1a89
ddsouz5 <ddsouz5@gmail.com>
2024-04-16 21:39:59 -0400

    validation check to routes

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -102,7 +102,7 @@ app.use(morgan(function (tokens, req, res) {
     JSON.stringify(req.body)].join(' ')
 }))
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
   const body = request.body
 
   if (!body.name) {
@@ -133,20 +133,20 @@ app.post('/api/persons', (request, response) => {
 //
 //  response.json(person)
 
-  person.save().then(savedPerson => {
-    response.json(savedPerson)
-  })
+  person.save()
+    .then(savedPerson => {
+      response.json(savedPerson)
+    })
+  .catch(error => next(error))
 })
 
 app.put('/api/persons/:id', (request, response, next) => {
-  const body = request.body
+  const { name, number } = request.body
 
-  const person = {
-    name: body.name,
-    number: body.number
-  }
-
-  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+  Person.findByIdAndUpdate(
+    request.params.id, 
+    { name, number }, 
+    { new: true, runValidators: true, context: 'query' })
     .then(updatedPerson => {
       response.json(updatedPerson)
     })
@@ -158,6 +158,8 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name == 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name == 'ValidationError') {
+    return response.status(400).json({ error: error.message })
   }
   
   next(error)
