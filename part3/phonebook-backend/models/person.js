70516d0339981635efb915f3e36678ce68fbefeb
ddsouz5 <ddsouz5@gmail.com>
2024-04-21 15:54:33 -0400

    validation for number

--- part3/phonebook-backend/models/person.js
+++ part3/phonebook-backend/models/person.js
@@ -19,9 +19,20 @@ mongoose.connect(url)
 const personSchema = new mongoose.Schema({
   name: {
     type: String,
-    minLength: 3
+    minLength: 3,
+    required: true
   },
-  number: String,
+  number: {
+    type: String,
+    minLength: 8,
+    validate: {
+      validator: function(v) {
+        return /\d{2,3}-\d+/.test(v)
+      },
+      message: props => `${props.value} is not a valid phone number!`
+    },
+    required: true
+  }
 })
 
 personSchema.set('toJSON', {
